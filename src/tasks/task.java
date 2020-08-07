e0637d32b6655896dd6a02e97f8bc2ec04271cad
tatsuya.oishi <chooch0203@gmail.com>
2020-08-07 17:00:51 +0900

    Add pom.xml

--- src/tasks/task.java
+++ src/tasks/task.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
-public class task {
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
